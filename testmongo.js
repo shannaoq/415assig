c3b71967828e665fa51d9745f739b6c05ef41661
shannaoq <98989501+shannaoq@users.noreply.github.com>
2024-02-29 20:29:00 +0000

    changed db connection string

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://shanna:<password>@cluster0.esh0t93.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://shanna:mush@cluster0.esh0t93.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
